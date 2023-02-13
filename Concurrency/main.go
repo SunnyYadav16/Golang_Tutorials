abdfab24528eff89d90bdd6f8c35b1ecca7159b1
Sunny Yadav <sunny.y@simformsolutions.com>
2023-02-13 12:07:52 +0530

    Added Concurrency Module

--- Concurrency/main.go
+++ Concurrency/main.go
@@ -1,12 +1,43 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+	"sync"
+)
+
+var wg sync.WaitGroup
+
 func main() {
-	go greeter("Hello")
-	greeter("World")
+
+	var endpoints = []string{
+		"https://www.google.com",
+		//"https://www.fb.com",
+		"https://www.twitter.com",
+		"https://www.instagram.com",
+		"https://www.youtube.com",
+	}
+
+	for _, endpoint := range endpoints {
+		go getStatusCode(endpoint)
+		wg.Add(1)
+	}
+
+	wg.Wait()
 }
 
-func greeter(s string) {
-	for i := 0; i < 5; i++ {
-		println(s)
+//func greeter(s string) {
+//	for i := 0; i < 5; i++ {
+//		println(s)
+//	}
+//}
+
+func getStatusCode(endpoint string) {
+	defer wg.Done()
+
+	resp, err := http.Get(endpoint)
+	if err != nil {
+		fmt.Println(err)
 	}
+	fmt.Printf("%d status code for %s\n", resp.StatusCode, endpoint)
 }
